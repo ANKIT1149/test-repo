40f5fd385f0186dad2c668cd8dfb794bcef29694
ANKIT1149 <140632363+ANKIT1149@users.noreply.github.com>
2025-09-22 13:19:18 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,8 +1,8 @@
 function hello(name) {
   console.log("Hello " + name)
-  const a=90
-  const b=2140
-  let c=3050
+  const a=300
+  const b=140
+  let c=4050
   let d=9350
   console.log(a+b, c*d)
   console.log("Here is the calculation is done")
